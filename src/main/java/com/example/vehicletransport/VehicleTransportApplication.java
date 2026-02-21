7be37ede570af43c87e08055c608535b89b841cb
yaswanth0702 <yaswanthpodapati27@gmail.com>
2026-02-21 00:45:07 -0600

    Initialize VehicleTransportApplication with Spring Boot

--- src/main/java/com/example/vehicletransport/VehicleTransportApplication.java
+++ src/main/java/com/example/vehicletransport/VehicleTransportApplication.java
@@ -1,1 +1,11 @@
+package com.example.vehicletransport;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTransportApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VehicleTransportApplication.class, args);
+  }
+}
